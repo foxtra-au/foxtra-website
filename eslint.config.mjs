6ff51ee6fae96cb8f49587516a7250f37bac21d7
foxtra-au <rmekala@foxtra.ai>
2025-12-29 12:56:16 +1100

    updated the Next JS version to 16.1.0

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
+
