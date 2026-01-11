7d970eb1d61285e1f64a3e45a359c19966eff6aa
foxtra-au <rmekala@foxtra.ai>
2026-01-11 16:57:58 +1100

    updated tobot.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,22 @@ const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.foxtra.com.au';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/login/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/login/',
+          '/admin/',
+          // Block all Next.js static assets and build files
+          '/_next/',
+          // Block favicon and manifest files (these are in HTML head but shouldn't be crawled as pages)
+          '/favicon.ico',
+          '/favicon/',
+        ],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
